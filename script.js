75e6c2fc1454596e05bc580d824b88cf1287c461
d8uv <d8uv@d8uv.org>
2010-03-15 19:18:20 -0800

    Got basic slide functionality

--- script.js
+++ script.js
@@ -1,4 +1,37 @@
 "use strict";
 
 $(document).ready(function () {
+    var slidepos, LEFT, RIGHT;
+    slidepos = 0;
+    LEFT = -1
+    RIGHT = 1
+
+    //Initial Positioning
+    $("div").each(function () {
+        $(this).show();
+        $(this).offset({left: slidepos});
+        slidepos += $(window).width();
+    });
+    slidepos = 0;
+
+
+    function moveSlider(direction) {
+        slidepos += direction * $(window).width();
+        $(window).scrollLeft(slidepos);
+    }
+
+
+    $(window).click(function() {
+            moveSlider(RIGHT);
+    });
+
+    $(window).keydown(function (e) {
+        if (e.keyCode === 37) {
+            moveSlider(LEFT);
+        }
+        else if (e.keyCode === 39) {
+            moveSlider(RIGHT);
+        }
+    });
+
 });
