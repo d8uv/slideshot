06f22e76a2ed7d0024b00bfa869e6e01be1c6131
d8uv <d8uv@d8uv.org>
2010-03-30 13:34:24 -0800

    Added bounds checking

--- script.js
+++ script.js
@@ -1,28 +1,37 @@
 "use strict";
 
 $(document).ready(function () {
-    var slidepos, LEFT, RIGHT;
+    var slidepos, winwidth, LEFT, RIGHT;
     slidepos = 0;
-    LEFT = -1
-    RIGHT = 1
+    winwidth = $(window).width();
+    LEFT = -1;
+    RIGHT = 1;
 
     //Initial Positioning
     $("div").each(function () {
         $(this).show();
         $(this).offset({left: slidepos});
-        slidepos += $(window).width();
+        slidepos += winwidth;
     });
     slidepos = 0;
 
 
     function moveSlider(direction) {
-        slidepos += direction * $(window).width();
+        slidepos += direction * winwidth;
+
+        if (slidepos < winwidth) {
+            slidepos = 0;
+        }
+        if (slidepos > winwidth) {
+            slidepos = winwidth;
+        }
+
         $(window).scrollLeft(slidepos);
     }
 
 
     $(window).click(function() {
-            moveSlider(RIGHT);
+        moveSlider(RIGHT);
     });
 
     $(window).keydown(function (e) {
